384d055d305f38dac0d48c3bc859a518c9e2486c
GamingAstronamy <ethan-barnes@hotmail.com>
2025-06-14 21:42:02 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# HAMQTT
+
+ESP-IDF component for integrating devices with Home Assistant over MQTT
---
+++ include/HAMQTT/HAMQTT.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "hamqtt_device.h"
+
+// Components
+#include "hamqtt_binary_sensor.h"
+#include "hamqtt_button.h"
