6dc47711073d9c6553d2396a52738686f1441da7
GamingAstronamy <ethan-barnes@hotmail.com>
2025-06-15 19:58:27 -0500

    Seperated component into seperate internal and external headers

---
+++ include/HAMQTT/hamqtt_component_internal.h
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2025 Ethan Barnes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include "hamqtt_component.h"
+
+/* ----- V-Table (interface) ----- */
+
+struct HAMQTT_Component_VTable {
+    esp_err_t (*get_discovery_config)(HAMQTT_Component *component, 
+                                      cJSON *root,
+                                      const char *device_unique_id);
+
+    void (*handle_mqtt_message)(HAMQTT_Component *component,
+                                const char *topic,
+                                int topic_len,
+                                const char *data,
+                                int data_len);
+
+    void (*update)(HAMQTT_Component *component,
+                   esp_mqtt_client_handle_t mqtt_client);
+
+    const char *(*get_unique_id)(HAMQTT_Component *component);
+    
+    const char *const *(*get_subscribed_topics)(HAMQTT_Component *component,
+                                                size_t *count);
+
+};
+
+/* ----- Base Object ----- */
+
+struct HAMQTT_Component {
+    const HAMQTT_Component_VTable *v;
+
+    const char *subscribed_topics[HAMQTT_COMPONENT_MAX_TOPICS];
+    size_t      subscribed_topics_count;
+};
+
+/* ----- Inline dispatch helpers ----- */
+
+esp_err_t hamqtt_component_get_discovery_config(
+        HAMQTT_Component *c, cJSON *root, const char *device_uid)
+{
+    return c->v->get_discovery_config(c, root, device_uid);
+}
+
+void hamqtt_component_handle_mqtt_message(
+        HAMQTT_Component *c, const char *topic, int topic_len, const char *data, int data_len)
+{
+    c->v->handle_mqtt_message(c, topic, topic_len, data, data_len);
+}
+
+void hamqtt_component_update(
+        HAMQTT_Component *c, esp_mqtt_client_handle_t client)
+{
+    c->v->update(c, client);
+}
+
+const char *hamqtt_component_get_unique_id(
+        HAMQTT_Component *c)
+{
+    return c->v->get_unique_id(c);
+}
+
+const char * const *hamqtt_component_get_subscribed_topics(
+        HAMQTT_Component *c, size_t *count)
+{
+    if (c->v->get_subscribed_topics)
+        return c->v->get_subscribed_topics(c, count);
+
+    if (c->subscribed_topics_count > 0) {
+        *count = c->subscribed_topics_count;
+        return c->subscribed_topics;
+    }
+    *count = 0;
+    return NULL;
+}
