ee88f6ce7789d7035122e78dc64140c6c6ac2dec
Eden Barnes <ethan-barnes@hotmail.com>
2025-06-19 19:21:20 -0500

    Docs: Regenerated

--- docs/html/struct_h_a_m_q_t_t___button.js
+++ docs/html/struct_h_a_m_q_t_t___button.js
@@ -2,5 +2,5 @@ var struct_h_a_m_q_t_t___button =
 [
     [ "hamqtt_button_create", "struct_h_a_m_q_t_t___button.html#a1f9447d599145bec8b434e36b14b5c2f", null ],
     [ "hamqtt_button_destroy", "struct_h_a_m_q_t_t___button.html#afded2c0f3678e91422a2dd476253fc77", null ],
-    [ "hamqtt_button_get_config", "struct_h_a_m_q_t_t___button.html#ab7d013972129126007cc9c74de7e0d2e", null ]
+    [ "hamqtt_button_get_config", "struct_h_a_m_q_t_t___button.html#a532fca8676df15b0a4c7af5410b808a4", null ]
 ];
--- docs/html/struct_h_a_m_q_t_t___component.js
+++ docs/html/struct_h_a_m_q_t_t___component.js
@@ -3,6 +3,6 @@ var struct_h_a_m_q_t_t___component =
     [ "hamqtt_component_get_discovery_config", "struct_h_a_m_q_t_t___component.html#a076abb82f1d6bd547109139a0fd0477e", null ],
     [ "hamqtt_component_get_subscribed_topics", "struct_h_a_m_q_t_t___component.html#ab8daa7145718b9feb168f3819b750c0e", null ],
     [ "hamqtt_component_get_unique_id", "struct_h_a_m_q_t_t___component.html#ab84107885e9060b0ecf2d304fa6d81ec", null ],
-    [ "hamqtt_component_handle_mqtt_message", "struct_h_a_m_q_t_t___component.html#aa3b9a81bd5495e35b93e5a6ab233ae18", null ],
+    [ "hamqtt_component_handle_mqtt_message", "struct_h_a_m_q_t_t___component.html#a57313655092b106be18a619e2df3c246", null ],
     [ "hamqtt_component_update", "struct_h_a_m_q_t_t___component.html#ac64bc85cd566882dfc4473c76417c7c1", null ]
 ];
